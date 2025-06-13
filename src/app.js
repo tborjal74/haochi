2040b9a1622390e9a7340539847b01e88bca3f8b
tborjal <tborjal@byupathway.edu>
2025-06-13 18:47:23 +0800

    added response

--- src/app.js
+++ src/app.js
@@ -1,16 +1,7 @@
 const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-const connectDB = require('./config/db');
-connectDB();
-
-app.get('/', (req, res) => res.send('API is running'));
+app.use('/', require('./routes'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
